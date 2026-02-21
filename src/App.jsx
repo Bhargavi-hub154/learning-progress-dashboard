8f9f1fb24d054218a8609afdfaf0e44434a22678
Bhargavi-hub154 <2500030127@kluniversity.in>
2026-02-21 20:09:23 +0530

    idshfhi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 export default function App(){
   return(
     <div>
-      <p>Sathvika</p>
+      <p>Bhargavi is the grate soul and i feeel like she is the best in the world
+        Bhargavi is the grate soul and i feeel like she is the best in the world
+        Bhargavi is the grate soul and i feeel like she is the best in the world
+        Bhargavi is the grate soul and i feeel like she is the best in the world  
+      </p>
     </div>
   );
 }
